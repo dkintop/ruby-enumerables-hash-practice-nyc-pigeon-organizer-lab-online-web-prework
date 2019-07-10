8d16083aa7f10dc3e0b81269061d95abe818e2fb
Doug Kintop <dkintop@hotmail.com>
2019-07-10 20:05:46 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,9 +10,7 @@ def nyc_pigeon_organizer(data)
     end
     
     names.each do |name|         #this places names into names_hash as keys
-      names_hash[name][:color] = nil
-      names_hash[name][:gender] = nil
-      names_hash[name][:lives] = nil
+      
       
    binding.pry
    end
