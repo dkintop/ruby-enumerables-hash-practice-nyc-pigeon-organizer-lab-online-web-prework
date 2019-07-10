9fc4a4c251686f75f43b7220476e94abcd6cf4bf
Doug Kintop <dkintop@hotmail.com>
2019-07-10 19:30:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,12 +1,15 @@
 
 require 'pry'
 def nyc_pigeon_organizer(data)
-  names_hash = []
+  names_hash = {}
   names = nil
   
   data.each do |property, property_descriptor|
+    puts property
     names = property_descriptor.values.flatten
     end
+    names.each do
+      
     
     
     
