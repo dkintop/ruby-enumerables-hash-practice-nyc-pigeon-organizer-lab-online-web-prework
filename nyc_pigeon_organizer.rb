1f811c52c2da521d3420a482b6a0ff54376080e8
Doug Kintop <dkintop@hotmail.com>
2019-07-10 21:24:41 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,33 @@
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  names_hash = {}
+  data.each do |property, property_descriptor|  
+   
+    property_descriptor.each do |key, names|
+         names.each do |name|
+           if !names_hash[name]
+             names_hash[name] ={}
+           end
+           
+           if !names_hash[name][property]
+             names_hash[name][property] = []
+             
+         end
+         
+         names_hash[name][property] << key.to_s
+         
+         
+        end
+         end
+    
+    
+       
+    end
+    
+   
+   
+   names_hash 
+    
+    
+  
 end
