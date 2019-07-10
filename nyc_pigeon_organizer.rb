f3c021c26bd536fef152f01b17f3b5d70a59bcee
Doug Kintop <dkintop@hotmail.com>
2019-07-10 20:15:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,8 +9,8 @@ def nyc_pigeon_organizer(data)
     names = property_descriptor.values.flatten
     end
     
-    names.each do |name|         #this places names into names_hash as keys
-      names_hash[name] = {
+    names.each do |name|         #this places names into names_hash as keys                             and adds value keys to new_hash
+     names_hash[name] = {
         :gender => [nil],
         :color => [nil],
         :lives => [nil]
