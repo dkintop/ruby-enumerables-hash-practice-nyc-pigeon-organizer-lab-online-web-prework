73106b1aec1955278a462cc6a4b5d5ac5cec9d87
Doug Kintop <dkintop@hotmail.com>
2019-07-10 18:56:34 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,11 +1,13 @@
 
-#require 'pry'
+require 'pry'
 def nyc_pigeon_organizer(data)
+  names_array = []
   data.each do |property, property_descriptor|
     names = property_descriptor.values.flatten
+    if !names_array.inclde?
   
    
-#binding.pry
+binding.pry
 end
   
   
